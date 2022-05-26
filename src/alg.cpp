4d3a3064a74ae88be0f1b6c8eeae8c6eefd5592a
Abubyakirov-Timur <99200506+Abubyakirov-Timur@users.noreply.github.com>
2022-05-26 20:27:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,25 +5,27 @@
 #include  <cstdlib>
 #include  "bst.h"
 
+std::string low(std::string str) {
+  std::string t = "";
+  for (auto x = str.begin(); x != str.end(); ++x) {
+    t += tolower(*x);
+  }
+  return t;
+}
+
 BST<std::string> makeTree(const char* filename) {
   BST<std::string> newTree;
   std::ifstream file(filename);
   std::string word = "";
-  if (file.is_open()) {
-    while (!file.eof()) {
-      word = "";
-      while (true) {
-        char message = file.get();
-        if ((message >= 97 && message <= 122) || (message >= 65 && message <= 90)) {
-          word += tolower(message);
-        } else {
-          break;
-        }
-      }
+  while (!file.eof()) {
+    char mes = file.get();
+    if ((mes >= 'a' && mes <= 'z') || (mes >= 'A' && mes <= 'Z')) {
+      word += mes;
+    } else {
+      word = low(word);
       newTree.add(word);
+      word = "";
     }
-    return newTree;
-  } else {
-    throw std::string("Did`t open!");
   }
+  return newTree;
 }
